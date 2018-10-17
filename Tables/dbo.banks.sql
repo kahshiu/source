41324b95d76aa2e4a9b96b0d623a88cf4f2d61db
kahshiu@gmail.com <github123>
2018-10-17 11:19:57 +0800

    tttt

--- Tables/dbo.banks.sql
+++ Tables/dbo.banks.sql
@@ -4,7 +4,9 @@ CREATE TABLE [dbo].[banks]
 [vabank] [varchar] (100) COLLATE Latin1_General_CI_AI NULL,
 [ibank] [int] NULL,
 [vabacc] [varchar] (100) COLLATE Latin1_General_CI_AI NULL,
-[vabname] [varchar] (100) COLLATE Latin1_General_CI_AI NULL
+[vabname] [varchar] (100) COLLATE Latin1_General_CI_AI NULL,
+[vatest] [varchar] (max) COLLATE Latin1_General_CI_AI NULL,
+[vatest2] [int] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[banks] ADD CONSTRAINT [PK__banks__9FF5EEA23891A581] PRIMARY KEY CLUSTERED  ([irepcoid]) ON [PRIMARY]
